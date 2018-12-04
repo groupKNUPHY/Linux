7ad394d2d6cfc2497458608e0bcabde14d0466b6
jkim <jkim@localhost.localdomain>
2018-12-04 13:09:00 +0900

    Get xsec

---
+++ getXsec.sh
@@ -0,0 +1,4 @@
+cat Htozz/Events/run_01/run_01_tag_1_banner.txt | grep "Inte" | awk '{print $6}'
+cat Z+jets/Events/run_01/run_01_tag_1_banner.txt | grep "Inte" | awk '{print $6}'
+cat ZZto4l/Events/run_01/run_01_tag_1_banner.txt | grep "Inte" | awk '{print $6}'
+cat ttbar/Events/run_01/run_01_tag_1_banner.txt | grep "Inte" | awk '{print $6}'
