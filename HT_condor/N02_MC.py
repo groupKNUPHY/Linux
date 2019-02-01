b8f083c51e68b1bfe23083dcbb1024d2a2ead17d
ico1036 <jkim@localhost.localdomain>
2019-02-01 16:01:24 +0900

    HTcondor

---
+++ HT_condor/N02_MC.py
@@ -0,0 +1,20 @@
+import random
+import math
+
+N=1000000
+n=0
+
+for i in range(N):
+	
+	x = random.random()
+	y = random.random()
+	c = math.sqrt(x**2+y**2)
+
+	if c <= 1:
+		n+=1
+
+
+pi = 4*(float(n)/float(N))
+print(N ,n)
+print("Pi: {:.9f}".format(pi))
+
