c36a231a59128c584101bb953c1c672e6c6d76f1
ico1036 <jiwoong.kim@cern.ch>
2019-03-05 13:44:18 +0900

    condor_sampling

--- HT_condor/N03_makeJob.sh
+++ HT_condor/N03_makeJob.sh
@@ -1,31 +1,29 @@
 #!/bin/bash
 
-maxfile=5
-tag=$RANDOM
-tmp=tag_$tag
-mkdir $tmp
+maxfile=20
+tmp=condor_out
+if [ ! -d condor_out]; then mkdir $tmp; fi
 cp N03_sampling.sh $tmp/
+cp data.list $tmp
 
-nfile=`cat data.dat | wc -l`
+
+nfile=`cat data.list | wc -l`
 nfile=`expr $maxfile + $nfile -1`
 njob=`expr $nfile / $maxfile`
 
 cat << EOF > $tmp/job.jds
 executable = N03_sampling.sh
 universe = vanilla
-output   = $tmp/condorLog_\$(Cluster)_\$(Process).log
-error    = $tmp/condorLog_\$(Cluster)_\$(Process).log
+output   = $tmp/condorLog_\$(Process).log
+error    = $tmp/condorErr_\$(Process).log
 log      = /dev/null
 should_transfer_files = yes
 transfer_input_files = data.dat
 when_to_transfer_output = ON_EXIT
 transfer_output_files = condorOut
-requirements = (machine != "wn3015.sdfarm.kr") 
-requirements = \$(requirements) && (machine != "wn3021.sdfarm.kr") 
-requirements = \$(requirements) && (machine != "wn3004.sdfarm.kr") 
-requirements = \$(requirements) && (machine != "wn3014.sdfarm.kr") 
-requirements = \$(requirements) && (machine != "wn3012.sdfarm.kr") 
-arguments = \$(Process) ${outName}_\$(Cluster)_\$(Process).root $EleIdType $EnCorr $KinLLType $maxFile data.list
+arguments = \$(Process) $maxfile data.list
 queue $nJob
 EOF
 
+cd $tmp
+condor_submit job.jdl
