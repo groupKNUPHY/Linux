0bae805045a1636a708cd35f35c077960b94973c
ico1036 <jiwoong.kim@cern.ch>
2021-01-07 16:03:43 +0900

    gridpakc run

--- HT_condor/N04_Mg5withCondor.sh
+++ HT_condor/N04_Mg5withCondor.sh
@@ -7,7 +7,7 @@
 # You just edit:									  #
 # 1. gridpack name									  #
 # 2. Number of generate events of one gen			  #
-# 3. Number of queu									  #
+# 3. Number of queue								  #
 #													  #
 # If you choose 2. = 10000 and 3. = 5 				  #
 # Total # of events = 10000*5 = 50000				  #
@@ -20,18 +20,21 @@ gridpack=`readlink -e $1`
 if [ ! -f $gridpack ] || [ ! $1 ]; then echo "Error NotFound GridPack $1"; exit; fi
 
 
+N_gen=10000
+N_queue=5
+
 # Make excution file
 cat << EOF > runCondor.sh
 #!/bin/bash
 
 source /cvmfs/cms.cern.ch/cmsset_default.sh
-export SCRAM_ARCH=slc6_amd64_gcc630
-cd /x4/cms/ycyang/MadGraph/CMSSW_10_1_10
+export SCRAM_ARCH=slc7_amd64_gcc750
+cd /x5/cms/jwkim/ServiceWork/tmp/CMSSW_11_2_0_pre10/src
 eval \`scramv1 runtime -sh\`
 cd -
 
 tar xvf `basename $gridpack`
-./run.sh 10000 \$RANDOM
+./run.sh $N_gen \$RANDOM
 if [ ! -d condorOut ]; then mkdir condorOut; fi
 mv events.lhe.gz condorOut/events_\${1}_\${2}.lhe.gz
 EOF
@@ -49,12 +52,11 @@ should_transfer_files = yes
 transfer_input_files = $gridpack
 when_to_transfer_output = ON_EXIT
 transfer_output_files = condorOut
-requirements = (machine != "node06")
 arguments = \$(Cluster) \$(Process)
-queue 5
+queue $N_queue
 EOF
 if [ ! -d condorLog ]; then mkdir condorLog; fi
 
 # Submit job
 condor_submit job.jdl
-
+rm -rf runCondor.sh
